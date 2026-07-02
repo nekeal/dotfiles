092e9313154d6f0d660dd9a58bf786c2cfba18be
Szymon Cader <szymon.sc.cader@gmail.com>
2026-07-02 18:09:47 +0200

    Remove no longer used aliases

--- dot_zsh/executable_aliases.zsh
+++ dot_zsh/executable_aliases.zsh
@@ -1,17 +1,9 @@
 alias please='python manage.py'
-alias pi="pip install"
-alias pc="pip-compile"
-alias pcu="pip-compile --upgrade"
-alias pcup="pip-compile --upgrade-package"
-alias won="workon"
 alias ghpr="gh pr"
 alias ghprc="ghpr create"
 alias ghmru="ghpr update"
 alias ghrv="gh repo view"
 alias ghrvw="gh repo view --web"
-
-# poetry
-alias pos="poetry shell"
 
 #git
 alias gacc='ga CHANGELOG.md && gc CHANGELOG.md -m "Update CHANGELOG.md"'
